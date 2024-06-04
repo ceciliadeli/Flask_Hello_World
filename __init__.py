e99d719b8c957926519fd86d8a6c318415e233a1
ceciliadeli <163179034+ceciliadeli@users.noreply.github.com>
2024-06-04 19:28:32 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,11 +7,15 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return "<h2>Bonjour tout le monde Cecilia !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"
+    return "<h2>Bonjour tout le monde!</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"
 
 @app.route('/exercices/')
 def exercices():
     return render_template('exercices.html')
-                                                                                                               
+  
+@app.route("/contact/")
+def MaPremiereAPI():
+    return "<h2>Ma page de contact</h2>"  
+  
 if __name__ == "__main__":
   app.run(debug=True)
